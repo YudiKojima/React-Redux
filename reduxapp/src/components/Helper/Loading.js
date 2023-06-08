02044d31e2fb1bb31a50e1afd1842b7f180d2841
Marcos Yudi <marcosyudi96@hotmail.com>
2023-06-08 08:11:39 -0400

    loading and contents

---
+++ reduxapp/src/components/Helper/Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Loading.module.css";
+
+function Loading() {
+  return (
+    <>
+      <div className={styles.loading}></div>
+      <div className={styles.loading2}></div>
+      <div className={styles.loading3}></div>
+    </>
+  );
+}
+
+export default Loading;
